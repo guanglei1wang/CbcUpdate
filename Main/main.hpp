b3eb692a99635b43f998a2324d03a1667bef5faa
费玛 <guanglei.wgl@alibaba-inc.com>
2019-06-30 13:48:27 +0800

    libCbcSolver works;

---
+++ Main/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  cbcUpdate
+//
+//  Created by 王光磊 on 2019/6/28.
+//  Copyright © 2019 王光磊. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
